8c77be1411f2c5b43cd99d9c7dcae687ba110382
Eugene Lukin <eugene.lukin@gmail.com>
2017-03-31 05:06:43 +0300

    - layout fixed - icon added

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,12 +4,14 @@ import { Component } from "@angular/core";
   selector: "my-app",
   template: `
     <StackLayout>
-    <TextField hint="Email Address" keyboardType="email"
-      autocorrect="false" autocapitalizationType="none"></TextField>
-    <TextField hint="Password" secure="true"></TextField>
+      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+      <TextField hint="Email Address" keyboardType="email"
+        autocorrect="false" autocapitalizationType="none"></TextField>
+      <TextField hint="Password" secure="true"></TextField>
 
-    <Button text="Sign in"></Button>
-    <Button text="Sign up for Groceries"></Button>
+      <Button text="Sign in" class="submit-button"></Button>
+      <Button text="Sign up for Groceries"></Button>
+    </StackLayout>
   `,
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
