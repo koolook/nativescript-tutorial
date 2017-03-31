5578b804e75df214a9826c0c1ba3412ef5323d57
Eugene Lukin <eugene.lukin@gmail.com>
2017-03-31 04:58:14 +0300

    Layout

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
+    <StackLayout>
     <TextField hint="Email Address" keyboardType="email"
       autocorrect="false" autocapitalizationType="none"></TextField>
     <TextField hint="Password" secure="true"></TextField>
