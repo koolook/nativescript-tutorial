23c564c84bc89c51b36bf256765e1c3351f52edb
Eugene Lukin <eugene.lukin@gmail.com>
2017-03-31 05:39:13 +0300

    Test data bindings added

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,21 @@ import { Component } from "@angular/core";
   template: `
     <StackLayout>
       <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email"
+      <TextField hint="Email Address" keyboardType="email" [(ngModel)]="email"
         autocorrect="false" autocapitalizationType="none"></TextField>
       <TextField hint="Password" secure="true"></TextField>
 
-      <Button text="Sign in" class="submit-button"></Button>
+      <Button text="Sign in" class="submit-button" (tap)="submit()"></Button>
       <Button text="Sign up for Groceries"></Button>
     </StackLayout>
   `,
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  email = 'person@example.com';
+  submit() {
+    let msg = `Submit!  ${this.email}`;
+    console.log(msg);
+    alert(msg);
+  }
+}
