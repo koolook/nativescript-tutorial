aa9f9aede0fc273798d76c8a6dd8f2b799639ccc
Eugene Lukin <eugene.lukin@gmail.com>
2017-04-02 04:51:39 +0300

    Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,50 +1,7 @@
 import { Component } from "@angular/core";
-import { User } from "./shared/user/user";
-import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
-  providers: [UserService],
-  templateUrl: 'pages/login/login.html',
-  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+  template: "<page-router-outlet></page-router-outlet>"
 })
-export class AppComponent {
-  user: User;
-  isLoggingIn = true;
-
-  constructor(private userService : UserService) {
-    this.user = new User();
-    this.user.email = 'u1@example.com';
-    this.user.password = 'qwerty';
-    // this.user = <User>{email: 'person@example.com', password: 'let-me-pass'};
-  }
-
-  submit() {
-    if (this.isLoggingIn) {
-      this.login();
-    } else {
-      this.signUp();
-    }
-  }
-
-  login() {
-
-  }
-
-  signUp() {
-    this.userService.register(this.user)
-      .toPromise()
-      .then(() => {
-        alert('Account created.');
-        this.toggleDisplay();
-      })
-      .catch(err => {
-        let errorString = err.toString();
-        alert(`Error! : ${errorString}`);
-      })
-  }
-
-  toggleDisplay() {
-    this.isLoggingIn = !this.isLoggingIn;
-  }
-}
+export class AppComponent {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from "@angular/core";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { AppComponent } from "./app.component";
+import { routes, navigatableComponents } from "./app.routing";
+
 
 @NgModule({
   imports: [
     NativeScriptModule,
     NativeScriptFormsModule,
-    NativeScriptHttpModule
+    NativeScriptHttpModule,
+    NativeScriptRouterModule.forRoot(routes)
   ],
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ...navigatableComponents
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
